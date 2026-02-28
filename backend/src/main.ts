9e14027c24766be53369ee909a0b7caeea19de16
Your Name <your.email@example.com>
2026-02-28 15:27:00 +0530

    created prisma service with prisma client and implemented onmoduleinit, destroy and clean database( clean database is only for development). astablised connection. created  app.setGlobalPrefix('api/v1');

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //project description
+  app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch(
+  (error) => {
+    // eslint-disable-next-line prettier/prettier
+    Logger.error("Error starting server",error);
+    process.exit(1);
+  }
+);
