b7464b23bdaa2d4de61fd9b1b192704f2cf10995
Your Name <your.email@example.com>
2026-03-02 03:02:52 +0530

    Bug Fixed

--- backend/src/modules/auth/strategies/jwt.strategy.ts
+++ backend/src/modules/auth/strategies/jwt.strategy.ts
@@ -11,7 +11,7 @@ import { ExtractJwt, Strategy } from "passport-jwt";
 import { ConfigService} from "@nestjs/config";
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy){
+export class JwtStrategy extends PassportStrategy(Strategy,'jwt'){
     constructor(
         private prisma: PrismaService, 
         private configService : ConfigService
